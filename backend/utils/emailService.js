4e8f271b31369422a832ff7925de343b10000424
yashuupednekar <yashpednekar89@gmail.com>
2026-06-25 14:24:52 +0530

    Add dotenv configuration to emailService
    Load environment variables from .env file for email service.

--- backend/utils/emailService.js
+++ backend/utils/emailService.js
@@ -1,5 +1,7 @@
 const nodemailer = require('nodemailer');
+const dotenv = require('dotenv');
 
+dotenv.config();
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
